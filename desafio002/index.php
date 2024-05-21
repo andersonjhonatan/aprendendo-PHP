11a69a202a8de9dc082092394cd65672fce1029f
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2024-05-21 06:15:40 -0300

    feat: gerando um numero aleatorio

---
+++ desafio002/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Desafio 02 </title>
+</head>
+
+<body>
+
+  <?php
+  $min = 0;
+  $max = 100;
+
+  $num = rand($min, $max);
+
+  echo  "Gerando um numero aleatorio, entre o $min e $max... ";
+  echo "O numero aleatorio foi $num"
+
+
+  ?>
+
+</body>
+
+</html>
