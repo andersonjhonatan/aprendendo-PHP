fbd52d6ec856d082b8a9b9399a296f74c96f893c
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2024-05-21 06:23:19 -0300

    feat: funçoes para gerar numero aletorios

--- desafio002/index.php
+++ desafio002/index.php
@@ -13,10 +13,51 @@
   $min = 0;
   $max = 100;
 
+  /* ja aqui o rand ele funciona se o max é maior do que o minimo */
+
   $num = rand($min, $max);
 
   echo  "Gerando um numero aleatorio, entre o $min e $max... ";
   echo "O numero aleatorio foi $num"
+
+
+  ?>
+
+  <br />
+
+  <?php
+
+  $min = 0;
+  $max = 100;
+
+  /* o max aqui tem que ser mair do que oi minimo  */
+
+  $num = mt_rand($min, $max);
+
+  echo  "Gerando um numero aleatorio com o mt-rand poie ele é mais rapido , entre o $min e $max... ";
+  echo "O numero aleatorio foi $num"
+
+
+
+  ?>
+
+  <br />
+
+
+  <?php
+
+  $min = 0;
+  $max = 100;
+
+  /* randon int gera numeros aleatorioos criptografados seguros  */
+
+  /* so usar o random int para criptografar  */
+
+  $num = random_int($min, $max);
+
+  echo  "Gerando um numero criptografado random_int poie ele é muito lento, entre o $min e $max... ";
+  echo "O numero aleatorio foi $num"
+
 
 
   ?>
